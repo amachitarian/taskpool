de0bc601e5a1cd9956780c71c5734ea7d7f1aa94
Maurice <mcsmorenberg@gmail.com>
2016-02-23 11:16:43 +0100

    schema merge

--- db/schema.rb
+++ db/schema.rb
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 0) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-end
-||||||| merged common ancestors
-=======
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20160223091344) do
 
   # These are extensions that must be enabled in order to support this database
@@ -49,4 +14,3 @@ ActiveRecord::Schema.define(version: 20160223091344) do
   add_index "tasks", ["status"], name: "index_tasks_on_status", using: :btree
 
 end
->>>>>>> 91bf25d90ac3dc5af3da81cca87a11fe5b683467
