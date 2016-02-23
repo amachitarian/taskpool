91bf25d90ac3dc5af3da81cca87a11fe5b683467
amachitarian <ashot_3@msn.com>
2016-02-23 11:13:15 +0100

    Basic

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,53 @@
+class TaskController < ApplicationController
+  def index
+    render json: {
+  meta: {
+    count: Task.count,
+    page: 0
+  },
+  tasks: Task.all
+}
+end
+
+  def create
+    if task = Task.create(task_params)
+      render json: { task: task }
+    else
+      render json: {
+        message: "Could not create Todo",
+        errors: task.errors,
+      }, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    task = task.find(params[:id])
+
+    if task.update(task_params)
+      render json: { task: task }
+    else
+      render json: {
+        message: "Could not update Task",
+        errors: task.errors,
+      }, status: :unprocessible_entity
+    end
+  end
+
+  def destroy
+    task = task.find(params[:id])
+
+    if task.destroy
+      render json: { task: nil }
+    else
+      render json: {
+        message: "Could not destroy Task, please try again",
+      }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :status)
+  end
+end
